88f89bd6ef44bd3e13a6c360329e4581982743e8
mihuwis <mihuwis@gmail.com>
2024-05-03 11:14:16 +0200

    Add Matrix 2D class

---
+++ Matrix2D/Matrix2D.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix2D
+{
+    internal class Matrix2D
+    {
+        private int A { get; }
+        private int B { get; }
+        private int C { get; }
+        private int D { get; }
+        public Matrix2D()
+        {
+            this.A = 1;
+            this.B = 0;
+            this.C = 1;
+            this.D = 0;
+        }
+
+        public Matrix2D(int a, int b, int c, int d)
+        {
+            A = a;
+            B = b;
+            C = c;
+            D = d;
+        }
+
+    }
+}
--- Matrix2D/obj/Debug/net8.0/Matrix2D.AssemblyInfo.cs
+++ Matrix2D/obj/Debug/net8.0/Matrix2D.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Matrix2D")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d3401b54d3b2158b66f605c0f9f1b403b292a4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92fe6ef91ce41034b879c2ab59468ebe6d55219f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Matrix2D")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Matrix2D")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
